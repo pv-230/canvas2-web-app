6f53b074c3273d37c5774f935ff82121ab9d9d30
pv-230 <81388791+pv-230@users.noreply.github.com>
2022-02-17 19:40:36 -0500

    Add a check for password confirmation

--- canvas2/static/scripts/signup.js
+++ canvas2/static/scripts/signup.js
@@ -1,17 +1,51 @@
-// Shows the message flash box when there are flashed messages
 const errorBox = document.querySelector('.error-box');
 const flashedMessage = document.querySelector('.flashed-message');
+const checkbox = document.getElementById('teacher-check');
+const teacherNotice = document.querySelector('.teacher-notice');
+const submitButton = document.querySelector('button');
+const password = document.getElementById('password');
+const passwordConfirm = document.getElementById('password-confirm');
+/** @type {HTMLFormElement} */
+const form = document.getElementById('signup-form');
+const errorMessage = document.querySelector('.error-message');
+
+// Shows the message flash box when there are flashed messages
 if (flashedMessage) {
   errorBox.toggleAttribute('hidden');
 }
 
 // Shows a notice when the teacher account checkbox is checked
-const checkbox = document.querySelector('#teacher-check');
 checkbox.addEventListener('change', (e) => {
-  const teacherNotice = document.querySelector('.teacher-notice');
   if (e.target.checked) {
     teacherNotice.removeAttribute('hidden');
   } else {
     teacherNotice.setAttribute('hidden', '');
+  }
+});
+
+// Checks passwords for sameness before form submission
+submitButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  isValid = form.checkValidity();
+  form.reportValidity();
+
+  if (isValid) {
+    if (password.value === passwordConfirm.value && password.value) {
+      form.submit();
+    } else {
+      password.style.cssText = 'border: 1px solid rgb(255, 0, 0, .5)';
+      passwordConfirm.style.cssText = 'border: 1px solid rgb(255, 0, 0, .5)';
+      errorMessage.textContent = 'Passwords are not the same';
+
+      if (errorBox.hasAttribute('hidden')) {
+        errorBox.toggleAttribute('hidden');
+      } else {
+        flashedMessage.textContent = '';
+      }
+
+      if (errorMessage.hasAttribute('hidden')) {
+        errorMessage.toggleAttribute('hidden');
+      }
+    }
   }
 });
