43a85863af559476dff96c154615a56566ca6dbb
pv-230 <81388791+pv-230@users.noreply.github.com>
2022-03-02 13:19:58 -0500

    Add more comments to files

--- canvas2/static/scripts/signup.js
+++ canvas2/static/scripts/signup.js
@@ -1,3 +1,4 @@
+// Global element variables
 const errorBox = document.querySelector('.error-box');
 const flashedMessages = [...document.querySelectorAll('.flashed-message')];
 const checkbox = document.getElementById('teacher-check');
@@ -37,14 +38,17 @@ submitButton.addEventListener('click', (e) => {
       errorMessage.textContent = 'Passwords are not the same';
 
       if (errorBox.hasAttribute('hidden')) {
+        // Shows the error box
         errorBox.toggleAttribute('hidden');
       } else {
-        for (flashedMessage of flashedMessages) {
+        // Clears any flashed messages so only the password error message shows
+        for (let flashedMessage of flashedMessages) {
           flashedMessage.textContent = '';
         }
       }
 
       if (errorMessage.hasAttribute('hidden')) {
+        // Shows the password error message inside of the error box
         errorMessage.toggleAttribute('hidden');
       }
     }
