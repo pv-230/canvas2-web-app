8a93c28028acb12c29cfceb124b36db7c7c1dd95
Shreay Patel <shreaypatel@yahoo.com>
2022-04-09 05:35:12 -0400

    import script integrated into setup

--- canvas2/plagiarism/assets/nltkImportScript.py
+++
@@ -1,9 +0,0 @@
-import nltk
-
-def getCorpora():
-    corpora = ['stopwords', 'brown', 'omw-1.4', 'wordnet']
-    for x in corpora:
-        nltk.download(x)
-
-if __name__ == "__main__":
-    getCorpora()
