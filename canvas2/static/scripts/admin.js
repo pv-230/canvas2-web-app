dddac906561cc25b24decf3844677a8350ad2659
pv-230 <81388791+pv-230@users.noreply.github.com>
2022-04-18 22:54:05 -0400

    Implement admin panel sidebar

--- canvas2/static/scripts/admin.js
+++ canvas2/static/scripts/admin.js
@@ -1,1 +1,67 @@
+'use strict';
 
+// Assignment management module
+(() => {
+  // Frames
+  const view = document.querySelector('.view', HTMLElement);
+
+  // Approval view
+  const approvals = document.createElement('div');
+  const appr_content = document.createElement('div');
+  appr_content.textContent = 'Approvals content';
+  approvals.appendChild(appr_content);
+  view.appendChild(approvals);
+
+  // Users view
+  const users = document.createElement('div');
+  const users_content = document.createElement('div');
+  users_content.textContent = 'Users content';
+  users.appendChild(users_content);
+
+  // Courses view
+  const courses = document.createElement('div');
+  const courses_content = document.createElement('div');
+  courses_content.textContent = 'Course content';
+  courses.appendChild(courses_content);
+
+  // Buttons
+  const sidebarBtns = [...document.querySelectorAll('.sidebar-btn')];
+
+  // State
+  let selected = sidebarBtns[0];
+
+  /**
+   * Switches to the selected admin view.
+   * @param {Event} e
+   */
+  const selectView = (e) => {
+    if (!e.target.classList.contains('selected')) {
+      // Highlights the selected menu button
+      e.target.classList.add('selected');
+      selected.classList.remove('selected');
+      selected = e.target;
+
+      // Switches to the new view.
+      if (view.firstChild) {
+        view.removeChild(view.firstChild);
+
+        switch (e.target.textContent) {
+          case 'Approvals':
+            view.appendChild(approvals);
+            break;
+          case 'Users':
+            view.appendChild(users);
+            break;
+          case 'Courses':
+            view.appendChild(courses);
+            break;
+        }
+      }
+    }
+  };
+
+  // Event listeners
+  sidebarBtns.forEach((btn) => {
+    btn.addEventListener('click', selectView);
+  });
+})();
